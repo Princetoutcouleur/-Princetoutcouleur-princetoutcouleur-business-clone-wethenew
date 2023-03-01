5489bb791ea749e3be9c9d1491d1b6a1085b7d4c
The princetoutcouleur <babathiam0000@gmail.com>
2023-03-01 12:41:22 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+// caroussel dans la section nouveautés
+let items = document.querySelectorAll(".nouveautes .carousel .carousel-item");
+
+items.forEach((el) => {
+  const minPerSlide = 4;
+  let next = el.nextElementSibling;
+  for (var i = 1; i < minPerSlide; i++) {
+    if (!next) {
+      // wrap carousel by using first child
+      next = items[0];
+    }
+    let cloneChild = next.cloneNode(true);
+    el.appendChild(cloneChild.children[0]);
+    next = next.nextElementSibling;
+  }
+});
+
+// caroussel dans la section Dunk Low
+let items1 = document.querySelectorAll(".dunk-low .carousel .carousel-item");
+
+items1.forEach((el) => {
+  const minPerSlide = 4;
+  let next = el.nextElementSibling;
+  for (var i = 1; i < minPerSlide; i++) {
+    if (!next) {
+      // wrap carousel by using first child
+      next = items1[0];
+    }
+    let cloneChild = next.cloneNode(true);
+    el.appendChild(cloneChild.children[0]);
+    next = next.nextElementSibling;
+  }
+});
+
+// caroussel dans la section Air jordan
+let items2 = document.querySelectorAll(".air-jordan .carousel .carousel-item");
+
+items2.forEach((el) => {
+  const minPerSlide = 4;
+  let next = el.nextElementSibling;
+  for (var i = 1; i < minPerSlide; i++) {
+    if (!next) {
+      // wrap carousel by using first child
+      next = items2[0];
+    }
+    let cloneChild = next.cloneNode(true);
+    el.appendChild(cloneChild.children[0]);
+    next = next.nextElementSibling;
+  }
+});
